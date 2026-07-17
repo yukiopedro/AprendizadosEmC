4a3e05165a1dedc83deae1b73d696544821c988b
pedro <yukio.pedro@outlook.com>
2026-07-16 22:34:59 -0300

    First exercises

---
+++ ATIVIDADE_01/atividade.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    char nome[20];
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+
+    char curso[50];
+    printf("Digite seu curso: ");
+    scanf("%s", curso);
+
+    char semestre[10];
+    printf("Digite seu semestre: ");
+    scanf("%s", semestre);
+
+    char hobby [50];
+    printf("Digite seu hobby: ");
+    scanf("%s", hobby);
+
+    printf("Prazer, eu sou %s, estou cursando %s no semestre %s e meu hobby é %s.\n", nome, curso, semestre, hobby);
+    return 0;
+
+    //Meta Pessoal
+
+    double salario, despesas, reserva, prazo;
+    
+
+}
